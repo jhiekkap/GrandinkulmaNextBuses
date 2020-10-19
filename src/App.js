00360fc25659c2ee465470170400e71d8eb093cf
jhiekkap <jari.hiekkapelto@gmail.com>
2020-10-19 22:02:26 +0300

    taulukko alulle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
 
@@ -43,14 +43,61 @@ export const grandinKulmaQuery = gql`
 
 function App() {
 
+  const [nextBuses, setNextBuses] = useState([])
+
   const getNextBuses = async () => {
     const result = await client.query({ query: grandinKulmaQuery })
-    console.log('QUERY RESULT: ', result.data.stops[0].stoptimesWithoutPatterns)
+    const stopTimes = result.data.stops[0].stoptimesWithoutPatterns
+    console.log('STOP TIMES: ', stopTimes)
+    setNextBuses(result.data.stops[0].stoptimesWithoutPatterns)
   }
+
+  console.log('NEXT BUSES', nextBuses)
 
   return (
     <div className="App">
-      <button onClick={() => getNextBuses()}>GET NEXT BUSES</button>
+      <button onClick={() => getNextBuses()}>HAE SEURAAVAT BUSSIT</button>
+      <table>
+        <thead>
+          <tr>
+            <td>
+              Linja
+            </td>
+            <td>
+              Reitti
+            </td>
+            <td>
+             Tosiaikainen
+            </td>
+            <td>
+              Aikataulun mukainen tuloaika
+            </td>
+            <td>
+              Aikataulun mukainen lähtöaika
+            </td>
+            <td>
+              Arvioitu tuloaika
+            </td>
+            <td>
+              Arvioitu lähtöaika
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {nextBuses.map((bus, i) => <tr key={i}>
+            <td>
+              {bus.trip.routeShortName}
+            </td>
+            <td>
+              {bus.trip.route.longName}
+            </td>
+            <td>
+              {bus.trip.realtime ? 'KYLLÄ' : 'EI'}
+            </td>
+          </tr>)}
+        </tbody>
+      </table>
+
     </div>
   );
 }
@@ -59,5 +106,4 @@ export default App;
 
 // new Date(1603054800000).toLocaleString('fi-FI')
 // * 1000!
-/*
- 
+
