ca1e3c4b1e6cf29b6e5e9bf6d791ddfced3a4f57
jhiekkap <jari.hiekkapelto@gmail.com>
2020-10-20 12:00:56 +0300

    perustaulukko valmis

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+import { useInterval } from './hooks'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -41,6 +42,8 @@ export const grandinKulmaQuery = gql`
 }
 `
 
+const getTime = (date) => date.split(' ')[4]
+
 function App() {
 
   const [nextBuses, setNextBuses] = useState([])
@@ -48,15 +51,22 @@ function App() {
   const getNextBuses = async () => {
     const result = await client.query({ query: grandinKulmaQuery })
     const stopTimes = result.data.stops[0].stoptimesWithoutPatterns
-    console.log('STOP TIMES: ', stopTimes)
-    setNextBuses(result.data.stops[0].stoptimesWithoutPatterns)
+    console.log(new Date(), 'STOP TIMES: ', stopTimes)
+    setNextBuses(stopTimes)
   }
+
+  useInterval(() => {
+    getNextBuses()
+  }, 2000)
+
 
   console.log('NEXT BUSES', nextBuses)
 
+
+
   return (
     <div className="App">
-      <button onClick={() => getNextBuses()}>HAE SEURAAVAT BUSSIT</button>
+      {/* <button onClick={() => getNextBuses()}>HAE SEURAAVAT BUSSIT</button> */}
       <table>
         <thead>
           <tr>
@@ -67,34 +77,78 @@ function App() {
               Reitti
             </td>
             <td>
-             Tosiaikainen
+              Tosiaikainen
             </td>
             <td>
               Aikataulun mukainen tuloaika
             </td>
             <td>
-              Aikataulun mukainen lähtöaika
+              Arvioitu tuloaika
             </td>
             <td>
-              Arvioitu tuloaika
+              Tuloaika myöhässä
+            </td>
+            <td>
+              Aikataulun mukainen lähtöaika
             </td>
             <td>
               Arvioitu lähtöaika
             </td>
+            <td>
+              Lähtöaika myöhässä
+            </td>
           </tr>
         </thead>
         <tbody>
-          {nextBuses.map((bus, i) => <tr key={i}>
-            <td>
-              {bus.trip.routeShortName}
-            </td>
-            <td>
-              {bus.trip.route.longName}
-            </td>
-            <td>
-              {bus.trip.realtime ? 'KYLLÄ' : 'EI'}
-            </td>
-          </tr>)}
+          {nextBuses.map((bus, i) => {
+            const serviceDayInMs = bus.serviceDay * 1000
+            //const localizedServiceDayInMs = serviceDayInMs //+ 3 * 60 * 60 * 1000
+            //const localizedServiceDay1 = new Date(serviceDayInMs).toLocaleString('fi-FI')
+            //const localizedServiceDay = new Date(localizedServiceDayInMs)
+            //console.log('SERVICE DAY IN FINNISH TIME', localizedServiceDay, localizedServiceDay1)
+            const scheduledArrival = getTime(new Date(serviceDayInMs + bus.scheduledArrival * 1000).toString())
+            console.log('SCHEDULED ARRIVAL', scheduledArrival)
+            const realtimeArrival = getTime(new Date(serviceDayInMs + bus.realtimeArrival * 1000).toString())
+            console.log('REAL TIME ARRIVAL', realtimeArrival)
+            const arrivalDelay = getTime(new Date(serviceDayInMs + bus.arrivalDelay * 1000).toString())
+            console.log('ARRIVAL DELAY ', arrivalDelay)
+            const scheduledDeparture = getTime(new Date(serviceDayInMs + bus.scheduledDeparture * 1000).toString())
+            console.log('SCHEDULED DEPARTURE', scheduledDeparture)
+            const realtimeDeparture = getTime(new Date(serviceDayInMs + bus.realtimeDeparture * 1000).toString())
+            console.log('REAL TIME DEPARTURE', realtimeDeparture)
+            const departureDelay = getTime(new Date(serviceDayInMs + bus.departureDelay * 1000).toString())
+            console.log('DEPARTURE DELAY ', departureDelay)
+              
+            return <tr key={i}>
+              <td>
+                {bus.trip.routeShortName}
+              </td>
+              <td>
+                {bus.trip.route.longName}
+              </td>
+              <td>
+                {bus.trip.realtime ? 'KYLLÄ' : 'EI'}
+              </td>
+              <td>
+                {scheduledArrival}
+              </td>
+              <td>
+                {realtimeArrival}
+              </td>
+              <td>
+                {arrivalDelay}
+              </td>
+              <td>
+                {scheduledDeparture}
+              </td>
+              <td>
+                {realtimeDeparture}
+              </td>
+              <td>
+                {departureDelay}
+              </td>
+            </tr>
+          })}
         </tbody>
       </table>
 
@@ -103,7 +157,4 @@ function App() {
 }
 
 export default App;
-
-// new Date(1603054800000).toLocaleString('fi-FI')
-// * 1000!
 
